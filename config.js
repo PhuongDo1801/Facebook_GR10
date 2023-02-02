b93e75914e0d10a8aaf98180140be916ec70ea08
PhamDinhMinh <phamdinhminh030801@gmail.com>
2023-02-02 14:16:04 +0700

    addCommit1

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
+import "firebase/compat/storage";
 
 export const firebaseConfig = {
   apiKey: "AIzaSyBFlgqVXfxOTM7wpmXhsJ5_TQUPiIeb7w0",
@@ -12,6 +13,16 @@ export const firebaseConfig = {
   measurementId: "G-5B3E3NP2BP",
 };
 
+// export const firebaseConfigImage = {
+//   apiKey: "AIzaSyBqwEUK3QUy3rPZy07SfqdVfupfXASLBK0",
+//   authDomain: "images-16563.firebaseapp.com",
+//   projectId: "images-16563",
+//   storageBucket: "images-16563.appspot.com",
+//   messagingSenderId: "126635534277",
+//   appId: "1:126635534277:web:0c275181ade06bfc694dac",
+//   measurementId: "G-NNLVS0YB0S",
+// };
+// firebase.initializeApp(firebaseConfigImage) ||
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
--- screen/posts/addPost.js
+++ screen/posts/addPost.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import {
   Text,
   StyleSheet,
@@ -10,7 +11,9 @@ import {
   Dimensions,
   Image,
   TouchableOpacity,
+  ActivityIndicator,
 } from "react-native";
+import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
 import { ImagePicker } from "expo-image-multiple-picker";
 import ThreePicture from "../../components/ThreePicture";
 import TwoPicture from "../../components/TwoPicture";
@@ -22,10 +25,12 @@ import { Ionicons } from "@expo/vector-icons";
 import { Entypo } from "@expo/vector-icons";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-function AddPost() {
+function AddPost({ navigation }) {
   const [vertical, setVertical] = useState(false);
   const [showImagePicker, setShowImagePicker] = useState(false);
   const [selectedImages, setSelectedImages] = useState([]);
+  const [text, onChangeText] = useState("");
+  const [loading, setLoaing] = useState(false);
 
   const closeImagePicker = (assets) => {
     setSelectedImages([...assets]);
@@ -45,129 +50,214 @@ function AddPost() {
     );
   };
 
+  const handlePost = async () => {
+    let responseImage = [];
+    for (let i = 0; i < selectedImages.length; i++) {
+      const fileName = "img-" + new Date().getTime();
+      const storage = getStorage();
+      const my_ref = ref(storage, `images/${fileName}.jpg`);
+      const metadata = {
+        contentType: "image/jpeg",
+      };
+      const img = await fetch(selectedImages[i].uri);
+      const bytes = await img.blob();
+      await uploadBytes(my_ref, bytes, metadata)
+        .then(async (res) => {
+          if (res.metadata) {
+            await getDownloadURL(my_ref)
+              .then((url) => {
+                imageUrl = url + ".jpg";
+                responseImage.push(url);
+              })
+              .catch((error) => console.log(error));
+          } else {
+            console.log("UPLOAD FILE ERROR!");
+          }
+        })
+        .catch((error) => console.log(error));
+    }
+    setLoaing(true);
+    const token = await AsyncStorage.getItem("id_token");
+    const potion = {
+      described: text,
+      images: responseImage,
+    };
+    return fetch("https://severfacebook.up.railway.app/api/v1/posts/create", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        authorization: "token " + token,
+      },
+      body: JSON.stringify(potion),
+    })
+      .then((response) => {
+        const statusCode = response.status;
+        if (statusCode === 200) {
+          return (response = response.json());
+        } else {
+          alert(
+            "Số điện thoại hoặc mật khẩu không chính xác\n " +
+              "Vui lòng đăng nhập lại"
+          );
+        }
+      })
+      .then((response) => {
+        if (response !== undefined) {
+          // console.log(response.data.described);
+          navigation.replace("Home", {
+            _id: response.data._id,
+          });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <View style={styles.container}>
       {showImagePicker && <ImagePickerContainer />}
-      <View style={styles.contentHeader}>
-        <View style={styles.contentHeaderLeft}>
-          <AntDesign name="arrowleft" size={28} color="black" />
-          <Text style={styles.textHeader}>Tạo bài viết</Text>
-        </View>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.textButton}>ĐĂNG</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.contentBody}>
-        <View style={styles.headerBody}>
-          <View style={styles.imageAvater}>
-            <Image
-              source={{
-                uri: "https://haycafe.vn/wp-content/uploads/2022/07/anh-wibu-co-gai-va-mat-na-cao.jpg",
-              }}
-              style={styles.avatar}
-            />
-          </View>
-          <View style={styles.infor}>
-            <Text style={styles.textInfor}>Pham Minh</Text>
-            <TouchableOpacity style={styles.inforBottom}>
-              <FontAwesome5 name="user-friends" size={16} color="#666" />
-              <Text style={styles.textinforBottom}>Bạn bè</Text>
+      {loading ? (
+        <ActivityIndicator
+          size="large"
+          color="#00ff00"
+          style={styles.loading}
+        />
+      ) : (
+        <View style={styles.container1}>
+          <View style={styles.contentHeader}>
+            <View style={styles.contentHeaderLeft}>
+              <AntDesign
+                name="arrowleft"
+                size={28}
+                color="black"
+                onPress={() => navigation.goBack()}
+              />
+              <Text style={styles.textHeader}>Tạo bài viết</Text>
+            </View>
+            <TouchableOpacity style={styles.button} onPress={handlePost}>
+              <Text style={styles.textButton}>ĐĂNG</Text>
             </TouchableOpacity>
           </View>
-        </View>
-        <View style={styles.betweenBody}>
-          <TextInput
-            style={styles.textInput}
-            multiline={true}
-            placeholder={
-              selectedImages !== []
-                ? "Hãy nói gì về bức ảnh này"
-                : "Bạn đang nghĩ gì? "
-            }
-          />
-        </View>
-        <View>
-          {selectedImages.length === 3 ? (
-            <ThreePicture selectedImages={selectedImages} />
-          ) : selectedImages.length === 2 ? (
-            <TwoPicture selectedImages={selectedImages} />
-          ) : selectedImages.length === 4 ? (
-            <FourPicture selectedImages={selectedImages} />
-          ) : selectedImages.length === 1 ? (
-            <Image
-              source={{ uri: selectedImages[0].uri }}
-              style={styles.picture}
-            />
-          ) : (
-            ""
-          )}
-        </View>
-      </View>
-      <View style={styles.footer}>
-        {vertical ? (
-          <View style={styles.iconFooterCol}>
-            <TouchableOpacity
-              style={styles.item}
-              onPress={() => setShowImagePicker((pre) => !pre)}>
-              <Ionicons
-                name="md-images"
-                size={24}
-                color="green"
-                style={styles.icon}
+          <View style={styles.contentBody}>
+            <View style={styles.headerBody}>
+              <View style={styles.imageAvater}>
+                <Image
+                  source={{
+                    uri: "https://haycafe.vn/wp-content/uploads/2022/07/anh-wibu-co-gai-va-mat-na-cao.jpg",
+                  }}
+                  style={styles.avatar}
+                />
+              </View>
+              <View style={styles.infor}>
+                <Text style={styles.textInfor}>Pham Minh</Text>
+                <TouchableOpacity style={styles.inforBottom}>
+                  <FontAwesome5 name="user-friends" size={16} color="#666" />
+                  <Text style={styles.textinforBottom}>Bạn bè</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View style={styles.betweenBody}>
+              <TextInput
+                style={styles.textInput}
+                multiline={true}
+                onChangeText={onChangeText}
+                value={text}
+                placeholder={
+                  selectedImages !== []
+                    ? "Hãy nói gì về bức ảnh này"
+                    : "Bạn đang nghĩ gì? "
+                }
               />
-              <Text>Ảnh/video</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.item}>
-              <Entypo
-                name="emoji-happy"
-                size={24}
-                color="#F5C33B"
-                style={styles.icon}
-              />
-              <Text>Cảm xúc/Hoạt động</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.item}>
-              <Entypo
-                name="camera"
-                size={24}
-                color="#0091ff"
-                style={styles.icon}
-              />
-              <Text>Camera</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.item}
-              onPress={() => setVertical(!vertical)}>
-              <MaterialCommunityIcons
-                name="dots-horizontal-circle"
-                size={24}
-                color="#555"
-                style={styles.icon}
-              />
-              <Text>Ẩn</Text>
-            </TouchableOpacity>
+            </View>
+            <View>
+              {selectedImages.length === 3 ? (
+                <ThreePicture selectedImages={selectedImages} />
+              ) : selectedImages.length === 2 ? (
+                <TwoPicture selectedImages={selectedImages} />
+              ) : selectedImages.length === 4 ? (
+                <FourPicture selectedImages={selectedImages} />
+              ) : selectedImages.length === 1 ? (
+                <Image
+                  source={{
+                    uri: selectedImages[0].uri,
+                  }}
+                  style={styles.picture}
+                />
+              ) : (
+                ""
+              )}
+            </View>
           </View>
-        ) : (
-          <View style={styles.iconFooter}>
-            <TouchableOpacity onPress={() => setShowImagePicker((pre) => !pre)}>
-              <Ionicons name="md-images" size={24} color="green" />
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <Entypo name="emoji-happy" size={24} color="#F5C33B" />
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <Entypo name="camera" size={24} color="#0091ff" />
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => setVertical(!vertical)}>
-              <MaterialCommunityIcons
-                name="dots-horizontal-circle"
-                size={24}
-                color="#555"
-              />
-            </TouchableOpacity>
+          <View style={styles.footer}>
+            {vertical ? (
+              <View style={styles.iconFooterCol}>
+                <TouchableOpacity
+                  style={styles.item}
+                  onPress={() => setShowImagePicker((pre) => !pre)}>
+                  <Ionicons
+                    name="md-images"
+                    size={24}
+                    color="green"
+                    style={styles.icon}
+                  />
+                  <Text>Ảnh/video</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.item}>
+                  <Entypo
+                    name="emoji-happy"
+                    size={24}
+                    color="#F5C33B"
+                    style={styles.icon}
+                  />
+                  <Text>Cảm xúc/Hoạt động</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.item}>
+                  <Entypo
+                    name="camera"
+                    size={24}
+                    color="#0091ff"
+                    style={styles.icon}
+                  />
+                  <Text>Camera</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.item}
+                  onPress={() => setVertical(!vertical)}>
+                  <MaterialCommunityIcons
+                    name="dots-horizontal-circle"
+                    size={24}
+                    color="#555"
+                    style={styles.icon}
+                  />
+                  <Text>Ẩn</Text>
+                </TouchableOpacity>
+              </View>
+            ) : (
+              <View style={styles.iconFooter}>
+                <TouchableOpacity
+                  onPress={() => setShowImagePicker((pre) => !pre)}>
+                  <Ionicons name="md-images" size={24} color="green" />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <Entypo name="emoji-happy" size={24} color="#F5C33B" />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <Entypo name="camera" size={24} color="#0091ff" />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => setVertical(!vertical)}>
+                  <MaterialCommunityIcons
+                    name="dots-horizontal-circle"
+                    size={24}
+                    color="#555"
+                  />
+                </TouchableOpacity>
+              </View>
+            )}
           </View>
-        )}
-      </View>
+        </View>
+      )}
     </View>
   );
 }
@@ -182,6 +272,14 @@ const styles = StyleSheet.create({
     paddingTop: StatusBar.currentHeight,
     backgroundColor: "#fff",
     flex: 1,
+  },
+  container1: {
+    backgroundColor: "#fff",
+    flex: 1,
+  },
+  loading: {
+    justifyContent: "center",
+    alignItems: "center",
   },
   contentHeader: {
     marginTop: 20,
@@ -204,14 +302,14 @@ const styles = StyleSheet.create({
     fontSize: 18,
   },
   button: {
-    backgroundColor: "#ccc",
+    backgroundColor: "#1B74E4",
     padding: 8,
     paddingHorizontal: 10,
     borderRadius: 8,
     marginBottom: 7,
   },
   textButton: {
-    opacity: 0.4,
+    color: "#fff",
     fontWeight: "600",
   },
   // Body
@@ -260,6 +358,11 @@ const styles = StyleSheet.create({
     fontSize: 22,
     marginRight: 10,
     fontWeight: "300",
+  },
+  picture: {
+    width: "100%",
+    height: 400,
+    overflow: "hidden",
   },
 
   //Body Footer
